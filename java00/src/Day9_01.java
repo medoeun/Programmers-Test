c159fbcdc71928b31ac29f9d8f62a7eca29353f5
Doeun <id5eun30@gmail.com>
2023-12-23 21:12:54 +0900

    2023.12.23 update

---
+++ java00/src/Day9_01.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Day9_01 {
+
+    //pcce 기출 1번 출력 : 변수 빈칸 채우기
+    // 결과
+    //Spring is beginning
+    //13
+    //310
+
+    public static void main(String[] args) {
+        String msg =
+                "Spring is beginning";
+        int val1 =
+                3;
+        String val2 =
+                "3";
+
+        System.out.println(msg);
+        System.out.println(val1 + 10);
+        System.out.println(val2 + "10");
+    }
+}
