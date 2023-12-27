212a140d35af4d341d1f2036d0ff3fd213449bc3
Doeun <id5eun30@gmail.com>
2023-12-27 23:04:18 +0900

    2023.12.27 update

---
+++ java00/src/test.java
@@ -0,0 +1,14 @@
+public class test {
+    public static void main(String[] args) {
+//        StringBuilder sb = new StringBuilder();
+//
+//        for (int i = 0; i < 3; i++) {
+//            sb.append("String").append("\n");
+//        }
+//
+//        String str = sb.toString();  // 형변환!!
+//        System.out.println(str);
+
+        System.out.println("String".repeat(3));
+    }
+}
